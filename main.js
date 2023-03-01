97e9a92ec413385a400b80c8f185906c86eb49c3
Fernanda Vázquez <106204371+EliVazc@users.noreply.github.com>
2023-02-28 19:04:08 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+var myButton = document.getElementById("mybot");
+var myButton2 = document.getElementById("mybot2");
+var myButton3 = document.getElementById("mybot3");
+
+
+		myButton.addEventListener("click", function(){
+			myButton.innerHTML = "¡Agregado al carrito!";
+			myButton.style.backgroundColor = "red";
+		});
+
+        myButton2.addEventListener("click", function(){
+			myButton2.innerHTML = "¡Agregado al carrito!";
+			myButton2.style.backgroundColor = "red";
+		});
+
+        myButton3.addEventListener("click", function(){
+			myButton3.innerHTML = "¡Agregado al carrito!";
+			myButton3.style.backgroundColor = "red";
+		});
